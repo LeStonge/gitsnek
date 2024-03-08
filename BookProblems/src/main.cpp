b553f1b459ebcee7bb01b502ea22d5f6501ffb9d
Benjamin John <Stingy1998@outlook.com>
2024-03-08 17:36:07 +0000

    Add project files.

---
+++ BookProblems/src/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include "Sales_item.h"
+
+
+int main()
+{
+/*
+	//Exercise 1.9
+	int x = 50;
+	int sum = 0;
+	
+	while (x >= 50 && x <= 100)
+	{
+		sum += x;
+		++x;
+	}
+	
+	std::cout << sum << std::endl;
+	
+	//Exercise 1.10
+	int y = 10;
+	while (y >= 0)
+	{
+		std::cout << y << std::endl;
+		--y;
+	}
+
+	//Exercise 1.11
+
+	int a, b;
+	std::cout << "Enter two numbers" << std::endl;
+
+	std::cin >> a >> b;
+
+	if (a < b)
+	{
+		while (a <= b)
+		{
+			std::cout << a << std::endl;
+			++a;
+		}
+	}
+	else
+	{
+		while (b <= a)
+		{
+			std::cout << b << std::endl;
+			++b;
+		}
+	}
+
+	//Exercise 1.12
+
+	int sum = 0;
+
+	for (int i = -100; i <= 100; i++)
+	{
+		sum += i;
+	}
+
+	std::cout << sum;
+
+	//Exercise 1.13
+
+	int sum = 0;
+
+	for (int i = 50; i <= 100; i++)
+	{
+		sum += i;
+	}
+
+	int sum = 0;
+
+	for (int i = 10; i >= 0; i--)
+	{
+		sum += i;
+	}
+
+	int val = 0;
+	int sum = 0;
+	std::string end;
+	while (std::cin >> val)
+	{
+		if (end == "end")
+			break;
+		sum += val;
+	}
+	
+	std::cout << sum;
+	
+
+	//Exercise 1.20
+
+	Sales_item book;
+	std::cin >> book;
+	std::cout << book << std::endl;
+	
+	//Exercise 1.21
+
+	Sales_item item1, item2;
+	std::cin >> item1 >> item2;
+	std::cout << item1 + item2 << std::endl;
+*/
+	//Exercise 1.23
+}
